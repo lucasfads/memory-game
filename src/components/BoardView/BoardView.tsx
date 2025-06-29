1acaaa6762709500de175e1308ff31c72d1185c2
Lucas Fads <lucasfads@gmail.com>
2025-06-29 19:31:56 -0300

    feat: add styling for BoardView component and update card class assignment

--- src/components/BoardView/BoardView.tsx
+++ src/components/BoardView/BoardView.tsx
@@ -1,4 +1,5 @@
 import Board from '../../models/Board';
+import './BoardView.css';
 
 const BoardView = () => {
     const board = new Board(['A', 'B', 'C', 'D'], 4);
@@ -6,7 +7,7 @@ const BoardView = () => {
     return (
         <div className="board">
             {board.cards.map((card) => (
-                <div key={`${card.pairId}-${card.id}`}>
+                <div key={`${card.pairId}-${card.id}`} className='card'>
                     { card.content }
                 </div>
             ))}
