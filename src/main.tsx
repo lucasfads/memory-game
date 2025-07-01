c79070510f7a10cdab4a95e1eea39bb64a4905e7
Lucas Fads <lucasfads@gmail.com>
2025-06-30 22:11:34 -0300

    feat: update index.html title and remove unused favicon; add footer to main.tsx; enhance styling in App.css and BoardView.css

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,10 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <footer>
+      <div className="container">
+        Developed with React by <a href="https://lucasfads.com/">Lucas Fads</a>.
+      </div>
+    </footer>
   </StrictMode>,
 )
